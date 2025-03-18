ea02b0ea1ac429847bb9a6398a83caa87d6ab8bf
samuel ovadya <shmuel.ovadya@outlook.fr>
2025-03-18 22:48:47 +0200

    working setup

--- src/virtual-display-manager/xrandr_setup_3.sh
+++ src/virtual-display-manager/xrandr_setup_3.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-# Run the initial xrandr setup script to reset display settings
-# (disconnecting all displays except Virtual-1)
-./xrandr_setup_1.sh
+echo "Running initial xrandr setup..."
+./xrandr_setup_1.sh 
 
-# Add modes for Virtual-2 and Virtual-3 with resolution 1920x1080
-xrandr --addmode Virtual-2 1920x1080
-xrandr --addmode Virtual-3 1920x1080
+# Get the resolution of Virtual-1
+resolution=$(xrandr | grep -w "Virtual-1" | grep -oP '\d+x\d+' | head -1)
+width=$(echo "$resolution" | cut -d 'x' -f1)
+height=$(echo "$resolution" | cut -d 'x' -f2)
 
-# Set the framebuffer to support 5760x1080 (three 1920x1080 displays)
-xrandr --fb 5760x1080 --output Virtual-1 --panning 1920x1080/5760x1080
+# Check if we successfully retrieved the resolution
+if [[ -z "$width" || -z "$height" ]]; then
+    echo "Error: Failed to get resolution of Virtual-1"
+    exit 1
+fi
+
+echo "Detected Virtual-1 resolution: ${width}x${height}"
+
+# Calculate new framebuffer size (3x width of Virtual-1)
+new_fb_width=$((width * 3))
+new_fb_height=$height
+
+echo "Calculated framebuffer size: ${new_fb_width}x${new_fb_height}"
+
+# Add modes for Virtual-2 and Virtual-3 with the same resolution as Virtual-1
+cmd="xrandr --addmode Virtual-2 ${width}x${height}"
+echo "Executing: $cmd"
+eval "$cmd"
+
+cmd="xrandr --addmode Virtual-3 ${width}x${height}"
+echo "Executing: $cmd"
+eval "$cmd"
+
+# Set the framebuffer to accommodate all three displays
+cmd="xrandr --fb ${new_fb_width}x${new_fb_height} --output Virtual-1 --panning ${width}x${height}/${new_fb_width}x${new_fb_height}"
+echo "Executing: $cmd"
+eval "$cmd"
 
 # Position Virtual-2 to the right of Virtual-1
-xrandr --output Virtual-2 --mode 1920x1080 --right-of Virtual-1
+cmd="xrandr --output Virtual-2 --mode ${width}x${height} --right-of Virtual-1"
+echo "Executing: $cmd"
+eval "$cmd"
 
 # Position Virtual-3 to the right of Virtual-2
-xrandr --output Virtual-3 --mode 1920x1080 --right-of Virtual-2
+cmd="xrandr --output Virtual-3 --mode ${width}x${height} --right-of Virtual-2"
+echo "Executing: $cmd"
+eval "$cmd"
+
+echo "Virtual triple display setup complete!"
